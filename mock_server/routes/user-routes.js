ed0f9d5bd5211cc3f5c904273b7a336d58ee7197
Andrew Kolansky <andrew.kolansky@gmail.com>
2023-05-09 14:51:54 -0600

    set up some more user routes

--- mock_server/routes/user-routes.js
+++ mock_server/routes/user-routes.js
@@ -3,25 +3,29 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
-
+const { User, userSchema} = require('../schema/user-schema');
 
 router.post(
-  '/register',
+  'api/users/register',
   [
     check('email', 'Email is required').isEmail(),
     check('password', 'Password must be at least 6 characters long').isLength({ min: 6 }),
   ],
   async (req, res) => {
+    console.log(req.body);
+    console.log('route hit');
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { email, password } = req.body;
+    const email = req.body.email;
+    const password = req.body.password;
 
     try {
       // Check if user already exists
-      let user = await User.findOne({ email });
+      let user = await userSchema.User.findOne({ email });
+      console.log(user);
 
       if (user) {
         return res.status(400).json({ msg: 'User already exists' });
@@ -53,5 +57,56 @@ router.post(
     }
   }
 );
+
+router.post('/api/users/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    // Create and sign JWT
+    const payload = { user: { id: user.id } };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 3600 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// Get many Users
+// will authenticate this a litte later to hide specific information.
+router.post('/api/users', async (req, res) => {
+  console.log('hit get all users route');
+  const lim = req.body.limit;
+  User.find().limit(lim).then((users)=> {
+    console.log(users);
+  })
+});
+
+//Get one User
+// will authenticate this a litte later to hide specific information.
+router.get('/api/users/:email', async (req, res) => {
+  const email = req.params.email;
+  let user = await userSchema.User.findOne({ email });
+  console.log(user);
+});
 
 module.exports = router;
--- mock_server/schema/user-schema.js
+++ mock_server/schema/user-schema.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
+// const schema = require('mongoose-geojson-schema');
+
 const userSchema = new mongoose.Schema({
   type: 'object',
   required: ['username', 'password', 'email'],
   properties: {
-    username: { type: string },
-    password: {type: string},
-    email: { type: string},
-    userType: { type: string },
+    username: { type: String },
+    password: {type: String},
+    email: { type: String},
+    userType: { type: String },
     created: { type: Date, default: Date.now },
     lastActive: { type: Date, default: Date.now },// not sure if to put a default value.
-    location: { type: 'Point' }
+    // location: { type: Schema.Types.Point, coordinates: [Number], index:'2dsphere' }
   }
 });
 
 
 const User = mongoose.model('User', userSchema);
+
+module.exports = { User, userSchema };
