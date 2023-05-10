9b90746bcbddc3a11f40f28d1411a456dc9cafb1
Andrew Kolansky <andrew.kolansky@gmail.com>
2023-05-09 20:19:20 -0600

    Working on the Header and Nav Elements

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent {
-
+  public projectsIsOpen = false;
+  toggleProjects() {
+    this.projectsIsOpen = !this.projectsIsOpen;
+  }
 }
