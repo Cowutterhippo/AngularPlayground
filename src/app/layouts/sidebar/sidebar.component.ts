920118c5ee86920371c51c5d98283e0b880829c3
andrew Mac <andrew.kolansky@gmail.com>
2023-06-03 20:44:52 -0400

    Side panel messing around

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,9 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  public open = false;
 
+  public open = false;
+  public searchInput = 'perform site search';
+
+  constructor(private router: Router) { }
   openSidebar() {
     this.open = !this.open;
   }
+
+  spSearch() {
+    // need to figure out a cool way to pass the search terms and display the results. 
+    // this is a novel way of doing it and seems like a pattern to explore.
+    this.router.navigate([{ outlets: { sidePanel: ['search'] } }]);
+  }
+
 }
